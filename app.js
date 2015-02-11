5099dac52bdbdf2da8610f83a2700b837f97c6ac
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-02-11 13:52:35 +0700

    init agi server

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var config = require('./config'),
+    agiServer = require('ding-dong'),
+    handler = require('./lib/handler');
+
+var debug = config['debug'];
+
+agiServer
+.createServer(function (context) {
+    handler(context, debug);
+})
+.listen(config.port);
+
+if (debug) {
+	console.log("server started");
+}
