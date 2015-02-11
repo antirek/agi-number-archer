f1910e6961d7af7a71fec2a57ada7e19ea0a348b
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-02-11 17:29:30 +0700

    move vars to config

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 module.exports = {
     port: 3000,
-    debug: true
+    debug: true,
+    mongo: {
+    	connectionString: 'mongodb://localhost/test',
+    	collection: 'test_insert'
+    },
+    asterisk: {
+    	agi_param_name: 'agi_arg_1',
+    	dialplan_var: 'REGION_CODE'
+    }
 };
