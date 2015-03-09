580abb66ca90a7b9af97e6f53b583f1b919634e0
sergey <serge.dmitriev@gmail.com>
2015-03-09 14:33:51 +0700

    fix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     asterisk: {
         agiParamName: 'agi_arg_1',
         resultDialPlanVarName1: 'REGION_CODE',
-        resultDialPlanVarName2: 'COUNTY_CODE'
+        resultDialPlanVarName2: 'COUNTY_CODE',
         beep: true
     },
     logger: {
